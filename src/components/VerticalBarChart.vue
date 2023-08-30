712840113319feab984b7a05d6d63efc69b16a8c
Bruck-3 <brucktafesse@yahoo.com>
2023-08-30 17:51:21 +0300

    per

--- src/components/VerticalBarChart.vue
+++ src/components/VerticalBarChart.vue
@@ -1,10 +1,10 @@
 <template>
   <div id="chart" class="chart-container">
     <div class="chart-column">
-      <select name="date" id="date" value="Mounth">
-        <option value="Per Day">Day</option>
-        <option value="Mounth">Mounth</option>
-        <option value="Per Day">Year</option>
+      <select name="date" id="date" value="Per Mounth">
+        <option value="Per Day">Per Day</option>
+        <option value="Per Mounth">Per Mounth</option>
+        <option value="Per Year">Per Year</option>
       </select>
       <apexchart
         type="bar"
